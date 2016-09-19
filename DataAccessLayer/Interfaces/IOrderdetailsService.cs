27073b8af19a25c5d9b1cd5da23d4850ed914b1c
sonny <sonny.van.kerchove@student.ehb.be>
2016-09-19 14:39:50 +0200

    All Interfaces Added

---
+++ DataAccessLayer/Interfaces/IOrderdetailsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    interface IOrderdetailsService
+    {
+        void Insert(Orderdetail orderdetail);
+        void Delete(Orderdetail orderdetail);
+
+        void Edit(Orderdetail orderdetail);
+
+        IEnumerable<Orderdetail> GetAll();
+
+        Orderdetail GetById(int orderdetailID); 
+    }
+}
