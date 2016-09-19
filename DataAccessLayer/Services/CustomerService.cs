2a724f2e0bfdc4b851d4313f484d1f7d059a488b
sonny <sonny.van.kerchove@student.ehb.be>
2016-09-19 15:42:32 +0200

    All Services implemented with methods: Insert, Delete, Edit, GetAll & GetById

--- DataAccessLayer/Services/CustomerService.cs
+++ DataAccessLayer/Services/CustomerService.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using DataAccessLayer.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer
+namespace DataAccessLayer.Services
 {
     public class CustomerService : ICustomerService
     {
@@ -29,17 +30,45 @@ namespace DataAccessLayer
 
         public void Edit(Customer customer)
         {
-            throw new NotImplementedException();
+            
+            using (var entities = new toysforboysEntities())
+            {
+                var originalCustomer = entities.customers.Find(customer.id);
+                originalCustomer.name = customer.name;
+                originalCustomer.streetAndNumber = customer.streetAndNumber;
+                originalCustomer.city = customer.city;
+                originalCustomer.state = customer.state;
+                originalCustomer.postalCode = customer.postalCode;
+                originalCustomer.countryId = customer.countryId;
+                entities.SaveChanges();
+            }
         }
 
-        public IEnumerable<Customer> GetAll()
+        public List<Customer> GetAll()
         {
-            throw new NotImplementedException();
+            List<Customer> AllCustomers = new List<Customer>();
+            using (var entities = new toysforboysEntities())
+            {
+                foreach (var customer in entities.customers)
+                {
+                    AllCustomers.Add(customer);
+                }
+            }
+
+            return AllCustomers;
         }
 
         public Customer GetById(int customerID)
         {
-            throw new NotImplementedException();
+            using (var entities = new toysforboysEntities())
+            {
+                var query = (from customer in entities.customers
+                            where customer.id == customerID
+                            orderby customer.id
+                            select customer).First();
+
+                return query;
+            }
         }
     }
 }
---
+++ DataAccessLayer/Services/OrderService.cs
@@ -0,0 +1,73 @@
+﻿using DataAccessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Services
+{
+    public class OrderService : IOrderService
+    {
+        public void Insert(Order order)
+        {
+            using (var entities = new toysforboysEntities())
+            {
+                entities.orders.Add(order);
+                entities.SaveChanges();
+            }
+        }
+
+        public void Delete(Order order)
+        {
+            using (var entities = new toysforboysEntities())
+            {
+                entities.orders.Remove(order);
+                entities.SaveChanges();
+            }
+        }
+
+        public void Edit(Order order)
+        {
+
+            using (var entities = new toysforboysEntities())
+            {
+                var originalOrder = entities.orders.Find(order.id);
+                originalOrder.orderDate = order.orderDate;
+                originalOrder.requiredDate = order.requiredDate;
+                originalOrder.shippedDate = order.shippedDate;
+                originalOrder.comments = order.comments;
+                originalOrder.customerId = order.customerId;
+                originalOrder.status = order.status;
+                entities.SaveChanges();
+            }
+        }
+
+        public List<Order> GetAll()
+        {
+            List<Order> AllOrders = new List<Order>();
+            using (var entities = new toysforboysEntities())
+            {
+                foreach (var order in entities.orders)
+                {
+                    AllOrders.Add(order);
+                }
+            }
+
+            return AllOrders;
+        }
+
+        public Order GetById(int orderID)
+        {
+            using (var entities = new toysforboysEntities())
+            {
+                var query = (from order in entities.orders
+                             where order.id == orderID
+                             orderby order.id
+                             select order).First();
+
+                return query;
+            }
+        }
+    }
+}
