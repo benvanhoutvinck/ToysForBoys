8fd9ba741248f84f85ee2cc673278fd9afcbdc7a
Joran Wuyts <joran_wuyts@hotmail.com>
2016-09-19 15:48:22 +0200

    MockProduct+Logincode

---
+++ WPFToysForBoys/ViewModel/ManagementVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using DataAccessLayer;
+
+namespace WPFToysForBoys.ViewModel
+{
+    public partial class ManagementVM:ViewModelBase
+    {
+        private IProductService pService;
+        public ManagementVM()
+        {
+            pService = new ProductServiceMock();
+            ProductList = pService.GetAll().ToList();
+            int i = 0;
+        }
+
+        private List<Product> productList;
+        public List<Product> ProductList
+        {
+            get { return productList; }
+            set
+            {
+                productList = value;
+                RaisePropertyChanged("ProductList");
+            }
+        }
+
+    }
+}
