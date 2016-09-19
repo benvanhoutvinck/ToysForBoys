9566b7adf680f1997444dc0610301d562c0b6e3a
sonny <sonny.van.kerchove@student.ehb.be>
2016-09-19 14:26:55 +0200

    Toevoegen Interface Product, customer & order

---
+++ DataAccessLayer/ICustomerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    interface ICustomerService
+    {
+        void Insert(Customer customer);
+        void Delete(Customer customer);
+
+        void Edit(Customer customer);
+
+        IEnumerable<Customer> GetAll();
+
+        Customer GetById(int customerID); 
+    }
+}
