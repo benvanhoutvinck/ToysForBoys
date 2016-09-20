eb406980e67c0a813b8c8f4f39db6db90eef98d7
Zeratul000 <keysers.diede@hotmail.com>
2016-09-20 13:56:39 +0200

    Linked WPF to DBA

---
+++ WPFToysForBoys/IdChecker.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFToysForBoys
+{
+    public class IdChecker
+    {
+        public static bool IdCheck(List<Product> l, Product p)
+        {
+            bool b = false;
+            foreach (Product product in l)
+            {
+                if (p.id == product.id)
+                {
+                    b = true;
+                    if (p == product)
+                        throw new ArgumentException();
+                    break;
+                }
+            }
+            return b;
+        }
+    }
+}
