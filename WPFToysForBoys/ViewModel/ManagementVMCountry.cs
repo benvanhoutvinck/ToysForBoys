dd62cdec66f05501078060bb414bf5429f316e72
stephan.lodewyckx <stephan.lodewyckx@PC24704>
2016-09-22 08:29:56 +0200

    Customer functionaliteit toevoegen

---
+++ WPFToysForBoys/ViewModel/ManagementVMCountry.cs
@@ -0,0 +1,67 @@
+﻿using DataAccessLayer;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFToysForBoys.ViewModel
+{
+    public partial class ManagementVM : ViewModelBase
+    {
+        private List<Country> countryList;
+        public List<Country> CountryList
+        {
+            get { return countryList; }
+            set
+            {
+                countryList = value;
+                RaisePropertyChanged("CountryList");
+            }
+        }
+
+        private List<Country> cCountryList;
+        public List<Country> CCountryList
+        {
+            get { return cCountryList; }
+            set
+            {
+                cCountryList = value;
+                RaisePropertyChanged("CCountryList");
+            }
+        }
+
+        private int selectedCCountryI;
+        public int SelectedCCountryI
+        {
+            get { return selectedCCountryI; }
+            set
+            {
+                selectedCCountryI = value;
+                RaisePropertyChanged("SelectedCCountryI");
+            }
+        }
+
+        private int selectedCountryI;
+        public int SelectedCountryI
+        {
+            get { return selectedCountryI; }
+            set
+            {
+                selectedCountryI = value;
+
+                if (value != -1)
+                {
+                    CustomerList = ccountryService.GetById(value).customers.ToList();
+                }
+                else
+                {
+                    CustomerList = cService.GetAll("country").ToList();
+                }
+
+                RaisePropertyChanged("SelectedCountryI");
+            }
+        }
+    }
+}
