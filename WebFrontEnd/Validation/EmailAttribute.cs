1a384457b3d311f5ac2b6e5994169c73cec73d91
Ben Vanhoutvinck <vanhoutvinckben@gmail.com>
2016-09-23 11:52:05 +0200

    added email validation

---
+++ WebFrontEnd/Validation/EmailAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace WebFrontEnd.Validation
+{
+    public class EmailAttribute : ValidationAttribute
+    {
+
+        public override bool IsValid(object value)
+        {
+            if (!(value is string))
+            {
+                return false;
+            }
+            // Return true if strIn is in valid e-mail format.
+            try
+            {
+                return Regex.IsMatch((string)value,
+                      @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                      @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                      RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}
