7249e9e9653735791fd00a9cfbe2c29c253b6775
Ben Vanhoutvinck <vanhoutvinckben@gmail.com>
2016-09-26 13:24:15 +0200

    tussentijdse commit

---
+++ WebFrontEnd/Views/Checkout/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFrontEnd.Views.Checkout
+{
+    public class CheckoutController : Controller
+    {
+        // GET: Checkout
+        public ActionResult ViewCart()
+        {
+            return View();
+        }
+    }
+}
