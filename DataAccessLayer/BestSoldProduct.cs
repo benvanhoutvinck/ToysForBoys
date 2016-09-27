73063703e22f06422f253d5f97bc49aa14e85b3e
Ramonvg <ramonvg@hotmail.be>
2016-09-27 15:51:14 +0200

    GetBestSoldProduct method toegevoegd aan ProductStatisticsService GEEFT NIEUWE KLASSE TERUG!!!

---
+++ DataAccessLayer/BestSoldProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace DataAccessLayer
+{
+    public class BestSoldProduct : Product
+    {
+        public int QuantityOrdered { get; set; }
+    }
+}
