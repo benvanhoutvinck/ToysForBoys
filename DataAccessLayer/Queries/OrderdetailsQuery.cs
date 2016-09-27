e28e70c58c4b090a9752882a8140da34c0e18045
Zeratul000 <keysers.diede@hotmail.com>
2016-09-27 11:24:31 +0200

    c

--- DataAccessLayer/Queries/OrderdetailsQuery.cs
+++ DataAccessLayer/Queries/OrderdetailsQuery.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
-    public class OrderQuery
-    {
-        public SortDateEnum? SortDateRange { get; set; }
-        public DateTime? DateRangeStart { get; set; }
-        public DateTime? DateRangeEnd { get; set; }
-        public int? CustomerId { get; set; }
-        public string Status { get; set; }
-        public SortDateEnum? SortDateCompareLeft { get; set; }
-        public char? DateCompareMode { get; set; }
-        public SortDateEnum? SortDateCompareRight { get; set; }
+    //public class OrderQuery
+    //{
+    //    public SortDateEnum? SortDateRange { get; set; }
+    //    public DateTime? DateRangeStart { get; set; }
+    //    public DateTime? DateRangeEnd { get; set; }
+    //    public int? CustomerId { get; set; }
+    //    public string Status { get; set; }
+    //    public SortDateEnum? SortDateCompareLeft { get; set; }
+    //    public char? DateCompareMode { get; set; }
+    //    public SortDateEnum? SortDateCompareRight { get; set; }
 
-    }
+    //}
 }
