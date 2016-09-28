a5e0fc78c52ba4c5251a14387c2ab971a82538e7
Zeratul000 <keysers.diede@hotmail.com>
2016-09-28 12:34:52 +0200

    DateRange validation added

--- WPFToysForBoys/ViewModel/OrderStatVM.cs
+++ WPFToysForBoys/ViewModel/OrderStatVM.cs
@@ -62,6 +62,94 @@ namespace WPFToysForBoys.ViewModel
             }
         }
 
+        private int orderCount;
+        public int OrderCount
+        {
+            get { return orderCount; }
+            set
+            {
+                orderCount = value;
+                RaisePropertyChanged("OrderCount");
+            }
+        }
+
+        private int customerCount;
+        public int CustomerCount
+        {
+            get { return customerCount; }
+            set
+            {
+                customerCount = value;
+                RaisePropertyChanged("CustomerCount");
+            }
+        }
+
+        private int disputedOrders;
+        public int DisputedOrders
+        {
+            get { return disputedOrders; }
+            set
+            {
+                disputedOrders = value;
+                RaisePropertyChanged("DisputedOrders");
+            }
+        }
+
+        private int shipments;
+        public int Shipments
+        {
+            get { return shipments; }
+            set
+            {
+                shipments = value;
+                RaisePropertyChanged("Shipments");
+            }
+        }
+
+        private int cancelled;
+        public int Cancelled
+        {
+            get { return cancelled; }
+            set
+            {
+                cancelled = value;
+                RaisePropertyChanged("Cancelled");
+            }
+        }
+
+        private decimal disputedOrdersPercentage;
+        public decimal DisputedOrdersPercentage
+        {
+            get { return disputedOrdersPercentage; }
+            set
+            {
+                disputedOrdersPercentage = value;
+                RaisePropertyChanged("DisputedOrdersPercentage");
+            }
+        }
+
+        private decimal totalRevenue;
+        public decimal TotalRevenue
+        {
+            get { return totalRevenue; }
+            set
+            {
+                totalRevenue = value;
+                RaisePropertyChanged("TotalRevenue");
+            }
+        }
+
+        private decimal averageRevenue;
+        public decimal AverageRevenue
+        {
+            get { return averageRevenue; }
+            set
+            {
+                averageRevenue = value;
+                RaisePropertyChanged("AverageRevenue");
+            }
+        }
+
         private DateTime dateRangeStart;
         public DateTime DateRangeStart
         {
@@ -89,6 +177,17 @@ namespace WPFToysForBoys.ViewModel
             }
         }
 
+        private bool dateBox;
+        public bool DateBox
+        {
+            get { return dateBox; }
+            set
+            {
+                dateBox = value;
+                RaisePropertyChanged("DateBox");
+            }
+        }
+
         private SortDateEnum? sortDateRange;
         public SortDateEnum? SortDateRange
         {
@@ -96,6 +195,10 @@ namespace WPFToysForBoys.ViewModel
             set
             {
                 sortDateRange = value;
+                if (value == null)
+                    DateBox = false;
+                else
+                    DateBox = true;
                 RaisePropertyChanged("SortDateRange");
             }
         }
@@ -223,6 +326,22 @@ namespace WPFToysForBoys.ViewModel
 
         private void Filter()
         {
+            if (SortDateRange != null)
+                if (this.DateRangeStart.CompareTo(this.DateRangeEnd) >= 0)
+                {
+                    Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("You have selected an invalid Date Range!", "Warning", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+            else
+                Refresh();
+
+        }
+
+        private void Refresh()
+        {
             List<Order> ol;
 
             if (string.IsNullOrWhiteSpace(Status) || "ALL".Equals(this.Status))
@@ -254,7 +373,7 @@ namespace WPFToysForBoys.ViewModel
                 });
             }
 
-            
+
 
             List<OrderStat> os = new List<OrderStat>();
 
@@ -262,6 +381,49 @@ namespace WPFToysForBoys.ViewModel
                 os.Add(new OrderStat(o));
 
             OrderStatList = os;
+
+
+            CalcStats(os);
+        }
+
+        private void CalcStats(List<OrderStat> OrderList)
+        {
+            this.OrderCount = OrderList.Count;
+
+            {
+                List<Customer> cl = new List<Customer>();
+
+                foreach (OrderStat os in OrderList)
+                    cl.Add(os.customer);
+
+                this.CustomerCount = cl.Distinct(new CustomerCompare()).Count();
+            }
+
+            this.DisputedOrders = OrderList.FindAll(o => o.status.Equals("DISPUTED")).Count;
+            if (this.OrderCount > 0)
+                this.DisputedOrdersPercentage = ((decimal)this.DisputedOrders) / this.OrderCount;
+            else
+                this.DisputedOrdersPercentage = 0;
+
+            {
+                decimal count = 0;
+
+                foreach (OrderStat os in OrderList.FindAll(o => !o.status.Equals("CANCELLED")))
+                    count += os.revenue;
+
+                this.TotalRevenue = count;
+            }
+            {
+                int count = OrderList.FindAll(o => !o.status.Equals("CANCELLED")).Count;
+
+                if (count > 0)
+                    this.AverageRevenue = this.TotalRevenue / count;
+                else
+                    this.AverageRevenue = 0;
+            }
+
+
+            this.Shipments = OrderList.FindAll(o => OrderStat.OverdueShipment(o.requiredDate, o.shippedDate)).Count;
         }
 
         private List<OrderStat> orderStatList;
